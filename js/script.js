f7aed85389c73b914cb11f6a102ae0fb32e2cbb3
Luca Faggianelli <luca.faggianelli@datalogic.com>
2016-02-09 18:43:38 +0100

    Move specific config to _data, fix active navigation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+---
+---
+
+var site_url = "{{ site.url }}";
+
 $(function() {
     initMenu();
 });
 
 function initMenu() {
-    var path = window.location.pathname;
+    var path = window.location;//.pathname;
     //path = path.replace(/\/$/, "");
     path = decodeURIComponent(path);
 
     $("#main-menu .menu-item a").each(function () {
         var href = $(this).attr('href');
-        if (path.substring(0, href.length) === href) {
+        href = href.replace('^'+site_url, '');
+        //if (path.substring(0, href.length) === href) {
+        if (path == href) {
             $(this).closest('li').addClass('active');
         }
     });
