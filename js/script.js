02fcb7d620b11f7b32869cd17cae6c7b8a35e5b6
Luca Faggianelli <luca.faggianelli@datalogic.com>
2016-02-18 13:32:52 +0100

    Screenshots in SDK page, added page layout

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var site_url = "{{ site.url }}";
 
 $(function() {
     initMenu();
+    initTOC();
 });
 
 function initMenu() {
@@ -20,4 +21,54 @@ function initMenu() {
             $(this).closest('li').addClass('active');
         }
     });
+}
+
+/**
+ * Build the table of content (TOC) from h* elements, the elements
+ * should be h2,h3, while h1 is reserved to the page title
+ */
+function initTOC() {
+    var toc = $('.toc-container ul');
+    var curr_level = 10;
+    var i = 0, j = 0;
+
+    $('.main-content').find('h2,h3').each(function() {
+        var par = $(this);
+        var level = parseInt(this.nodeName.match(/h(\d)/i)[1]);
+        if (level < curr_level) {
+            console.log('new paragraph', par.text());
+            i += 1;
+            j = 1;
+        } else {
+            j += 1;
+        }
+        curr_level = level;
+
+        var li = $('<li></li>');
+        var link = $('<a href="#'+par.attr('id')+'">&nbsp;&nbsp;'+par.text()+'</a>');
+        if (j == 1) {
+            link.prepend(i + '');
+            li.addClass('toc-level-1');
+        } else {
+            link.prepend(i + '.' + j);
+            li.addClass('toc-level-2');
+        }
+
+        if (!par.attr('id')) {
+            li.css('text-decoration', 'line-through');
+        }
+
+        li.append(link);
+        toc.append(li);
+    });
+}
+
+function getQRCodeURL(data, size) {
+    if (!data)
+        return;
+
+    if (!size)
+        size = 150;
+
+    return 'https://api.qrserver.com/v1/create-qr-code/?size='+size+'x'+size+'&data='+data;
 }
