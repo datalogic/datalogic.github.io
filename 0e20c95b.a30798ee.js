849c02664b91ea3810504d6aab92ce1e74c14f40
Gitlab CI <cody.brookshear@datalogic.com>
2020-11-20 00:07:37 +0000

    Deploy website - based on 9b509da49e8bb4a920fc49a79ae8e329f23885ec

---
+++ 0e20c95b.a30798ee.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{63:function(s){s.exports=JSON.parse('{"allTagsPath":"/releases/tags","slug":"scan-2-deploy-studio","name":"scan2deploy-studio","count":2,"permalink":"/releases/tags/scan-2-deploy-studio"}')}}]);
