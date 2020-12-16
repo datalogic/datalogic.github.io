e16549a0589b8a6192d2b46f153cc7a1775746e6
Gitlab CI <cody.brookshear@datalogic.com>
2020-12-16 22:40:08 +0000

    Deploy website - based on b2284f87e873b4e1e92a62d97fe7ee4f657f53e2

---
+++ 941c3503.27e4d1f6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[69],{140:function(a){a.exports=JSON.parse('{"allTagsPath":"/releases/tags","slug":"xamarin","name":"xamarin","count":1,"permalink":"/releases/tags/xamarin"}')}}]);
---
+++ db5ce22c.b4129190.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[99],{169:function(a){a.exports=JSON.parse('{"allTagsPath":"/releases/tags","slug":"scan-2-deploy-android-schema","name":"scan2deploy-android-schema","count":3,"permalink":"/releases/tags/scan-2-deploy-android-schema"}')}}]);
