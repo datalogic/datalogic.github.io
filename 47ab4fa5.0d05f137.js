95348c0d054eccff09659cb021f539ed06bfa921
Gitlab CI (Shankar, Sachin) <sachin.shankar@datalogic.com>
2022-01-06 20:42:54 +0000

    Deploy website - based on e702dfb3452b9074a3c2c016e0a86db6c907a8fb

--- 47ab4fa5.0d05f137.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[63],{149:function(e){e.exports=JSON.parse('{"permalink":"/releases","page":1,"postsPerPage":10,"totalPages":5,"totalCount":41,"previousPage":null,"nextPage":"/releases/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
