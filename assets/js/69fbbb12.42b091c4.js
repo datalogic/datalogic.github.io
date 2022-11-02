65d23f94b20971ee23888209e9dd8f47e3912cef
Gitlab CI (Grundy, Gilbert) <gilbert.grundy@datalogic.com>
2022-11-02 22:43:54 +0000

    Deploy website - based on 83da1081cb89ca3212efd0227f2a7589d3cef7b8

---
+++ assets/js/69fbbb12.42b091c4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[3284],{60125:function(a){a.exports=JSON.parse('{"allTagsPath":"/releases/tags","slug":"/releases/tags/datalogic-android-sdk","name":"datalogic-android-sdk","count":10,"permalink":"/releases/tags/datalogic-android-sdk"}')}}]);
