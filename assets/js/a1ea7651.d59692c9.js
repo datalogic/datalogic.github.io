d342e9e03db785654edfadb253765cac604990da
Gitlab CI (Hill, Andrew) <andrew.hill@datalogic.com>
2022-11-21 23:46:35 +0000

    Deploy website - based on f0fa8162ebb305fe092d6d2b75ad7bd005c716d2

--- assets/js/a1ea7651.d59692c9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[4630],{65788:function(e){e.exports=JSON.parse('{"permalink":"/releases/page/6","page":6,"postsPerPage":10,"totalPages":6,"totalCount":56,"previousPage":"/releases/page/5","nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
