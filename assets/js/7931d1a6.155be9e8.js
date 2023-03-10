6c327d42ba1f1a724828c92be0aade4352adcfc8
Gitlab CI (Watkins, Patrick) <pat.watkins@datalogic.com>
2023-03-10 00:51:52 +0000

    Deploy website - based on 082f77e14f8fcd41f330e0ed0d33c12ac97274d7

---
+++ assets/js/7931d1a6.155be9e8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[3627],{89783:function(e){e.exports=JSON.parse('{"allTagsPath":"/releases/tags","slug":"/releases/tags/browser","name":"browser","count":4,"permalink":"/releases/tags/browser"}')}}]);
