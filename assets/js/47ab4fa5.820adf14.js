8b1bfa4dbe3da241796ecee1de386fb2c1747368
Gitlab CI (Freel, Eric) <eric.freel@datalogic.com>
2023-06-07 22:40:51 +0000

    Deploy website - based on de7bdfc18edcd57d7725bac1b8952da7b6976275

--- assets/js/47ab4fa5.820adf14.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[2317],{2353:function(e){e.exports=JSON.parse('{"permalink":"/releases","page":1,"postsPerPage":10,"totalPages":8,"totalCount":71,"nextPage":"/releases/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
