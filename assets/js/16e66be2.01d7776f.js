2b634826427521e269ff94b01f4c5e43d7d2ddd7
Gitlab CI (Watkins, Patrick) <pat.watkins@datalogic.com>
2023-11-28 21:34:14 +0000

    Deploy website - based on 059bae6f1006be9b8afdab1a6394c196b4372fa6

---
+++ assets/js/16e66be2.01d7776f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[6078],{75681:function(a){a.exports=JSON.parse('{"label":"scan2deploy-android","permalink":"/releases/tags/scan-2-deploy-android","allTagsPath":"/releases/tags","count":12}')}}]);
