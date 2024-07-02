bc7060efc959bf6cf84e235dacf2e7bfc7ea70bc
Gitlab CI (Grundy, Gilbert) <gilbert.grundy@datalogic.com>
2024-07-02 23:43:59 +0000

    Deploy website - based on d064b53b0b5aaf2262452b75cd557a5785d9ec93

---
+++ assets/js/d62f0cd9.fcf76e7e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[36265],{95748:(t,i,u)=>{u.r(i),u.d(i,{default:()=>o});u(67294);var a=u(16550),e=u(85893);const o=function(){return(0,e.jsx)(a.l_,{to:"/quickboard/overview"})}}}]);
