da81af977f7e922685c331cbe3ae434b72f70bbb
Gitlab CI (Mock, Bronson) <bronson.mock@datalogic.com>
2024-11-18 22:24:00 +0000

    Deploy website - based on 5747b4995237e884e111f1eb5cdb5ba35bbf72c9

--- assets/js/677852b7.c6937c57.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdatalogic_github_io=self.webpackChunkdatalogic_github_io||[]).push([[95140],{68019:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/releases/page/3","page":3,"postsPerPage":10,"totalPages":14,"totalCount":132,"previousPage":"/releases/page/2","nextPage":"/releases/page/4","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
